359b4a3bb87edc79df1eac61ec479325e0180955
broxB <philippbrockmann@gmail.com>
2022-06-02 20:53:47 +0200

    Database 2.0 - Remodel database and add enum types
    - merged cultivated_crop and cultivation
    - many-to-many relationships for fields and crops/fertilizations
    - added soil_sample as table
    - reworked repr methods

--- app/database/model.py
+++ app/database/model.py
@@ -15,10 +15,85 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import backref, relationship
 
 
-class CropType(enum.Enum):
-    catch_crop = 0  # Zwischenfrucht
-    main_crop = 1  # Hauptfrucht
-    second_crop = 2  # Zweitfrucht
+class CropType(enum.IntEnum):
+    catch_crop = 1  # Zwischenfrucht
+    main_crop = 2  # Hauptfrucht
+    second_crop = 3  # Zweitfrucht
+
+
+class RemainsType(enum.IntEnum):
+    remains = 1
+    no_remains = 2
+    frozen = 3
+    not_frozen_fall = 4
+    not_frozen_spring = 5
+
+
+class FieldType(enum.IntEnum):
+    grassland = 1
+    cropland = 2
+    grass_fallowland = 3
+    crop_fallowland = 4
+
+
+class MeasureType(enum.IntEnum):
+    fall = 1
+    spring = 2
+    first_first_n_fert = 10
+    first_second_n_fert = 11
+    first_n_fert = 12
+    second_n_fert = 13
+    third_n_fert = 14
+    fourth_n_fert = 15
+    first_base_fert = 16
+    second_base_fert = 17
+    third_base_fert = 18
+    fourth_base_fert = 19
+    lime_fert = 20
+    misc_fert = 21
+
+
+class SoilType(enum.IntEnum):
+    sand = 1
+    light_loamy_sand = 2
+    strong_loamy_sand = 3
+    sandy_to_silty_loam = 4
+    clayey_loam_to_clay = 5
+    moor = 6
+
+
+class HumusType(enum.IntEnum):
+    less_4 = 1
+    less_8 = 2
+    less_15 = 3
+    less_30 = 4
+    more_30 = 5
+
+
+class FertClass(enum.IntEnum):
+    organic = 1
+    mineral = 2
+
+
+class FertType(enum.IntEnum):
+    # organic
+    digestate = 1
+    slurry = 2
+    manure = 3
+    dry_manure = 4
+    compost = 5
+    # mineral
+    K = 10
+    N = 11
+    N_K = 12
+    N_P = 13
+    N_S = 14
+    N_P_K = 15
+    N_P_K_S = 16
+    P = 17
+    P_K = 18
+    lime = 19  # Kalk
+    misc = 20  # Sonstige
 
 
 Base = declarative_base()
@@ -30,10 +105,10 @@ field_cultivation = Table(
     Column("cultivation_id", Integer, ForeignKey("cultivation.cultivation_id")),
 )
 
-cultivation_fertilization = Table(
-    "cultivation_fertilization",
+field_fertilization = Table(
+    "field_fertilization",
     Base.metadata,
-    Column("cultivation_id", Integer, ForeignKey("cultivation.cultivation_id")),
+    Column("field_id", Integer, ForeignKey("field.field_id")),
     Column("fertilization_id", Integer, ForeignKey("fertilization.fertilization_id")),
 )
 
@@ -45,91 +120,89 @@ class Field(Base):
     suffix = Column("suffix", Integer)
     name = Column("name", String)
     area = Column("area", Float(asdecimal=True))
-    type = Column("type", String)
-    field_cult = relationship(
+    type = Column("type", Enum(FieldType))
+    cultivations = relationship(
         "Cultivation",
         secondary=field_cultivation,
-        back_populates="cult_field",
+        back_populates="fields",
+    )
+    fertilizations = relationship(
+        "Fertilization",
+        secondary=field_fertilization,
+        back_populates="fields",
     )
 
+    __table_args__ = (UniqueConstraint("prefix", "suffix", "name", "area", "type", name="active_fields"),)
+
     def __repr__(self):
-        return f"Field(id='{self.id}', name='{self.prefix}-{self.suffix} {self.name}', ha='{self.area:.2f}', cultivations='{[cult.year for cult in self.field_cult]}')"
+        return (
+            f"Field(id='{self.id}', name='{self.prefix:02d}-{self.suffix} {self.name}', "
+            f"ha='{self.area:.2f}', type='{self.type.name}', "
+            f"cultivations={[f'{cult.crop_type.name}: {cult.crop.name}' for cult in self.cultivations]}, "
+            f"fertilizations={[f'{fert.measure.name} -> {fert.cultivation.crop.name}: {fert.fertilizer.name}' for fert in self.fertilizations]})"
+        )
 
 
 class Cultivation(Base):
     __tablename__ = "cultivation"
     id = Column("cultivation_id", Integer, primary_key=True)
     year = Column("year", Integer)
-    field_id = Column("field_id", Integer, ForeignKey("field.field_id"))
-    crops = relationship("CultivatedCrop", backref=backref("cultivation"))
-    fertilizations = relationship(
-        "Fertilization",
-        secondary=cultivation_fertilization,
-        back_populates="cultivations",
-    )
-    cult_field = relationship(
-        "Field",
-        secondary=field_cultivation,
-        back_populates="field_cult",
-    )
-    __table_args__ = (
-        UniqueConstraint(
-            "cultivation_id", "field_id", "year", name="active_cultivations"
-        ),
-    )
-
-    def __repr__(self):
-        return f"Cultivation(id='{self.id}', year='{self.year}', field='{[field.name for field in self.cult_field]}', crops='{[crop.crop.name for crop in self.crops]}')"
-
-
-class CultivatedCrop(Base):
-    __tablename__ = "cultivated_crop"
-    id = Column("cultivated_crop_id", Integer, primary_key=True)
-    cultivation_id = Column(
-        "cultivation_id", Integer, ForeignKey("cultivation.cultivation_id")
-    )
     crop_type = Column("crop_type", Enum(CropType))
     crop_id = Column("crop_id", Integer, ForeignKey("crop.crop_id"))
     crop_yield = Column("yield", Float(asdecimal=True))
-    remains = Column("remains", Boolean)
+    remains = Column("remains", Enum(RemainsType))
     legume_rate = Column("legume_rate", String)
-    crop = relationship("Crop", backref=backref("cultivated_crop"))
+    fields = relationship(
+        "Field",
+        secondary=field_cultivation,
+        back_populates="cultivations",
+    )
+    crop = relationship("Crop", backref=backref("cultivation"))
+
+    __table_args__ = (UniqueConstraint("year", "crop_type", name="active_crops"),)
 
     def __repr__(self):
-        return f"CultivatedCrop(id='{self.id}', type='{self.crop_type}', name='{self.crop.name}', yield='{self.crop_yield:.2f}')"
+        return (
+            f"Cultivation(id='{self.id}', year='{self.year}', type='{self.crop_type.name}', "
+            f"name='{self.crop.name}', yield='{self.crop_yield:.2f}', field='{self.fields[0].name}')"
+        )
+
+
+class Fertilization(Base):
+    __tablename__ = "fertilization"
+    id = Column("fertilization_id", Integer, primary_key=True)
+    cultivation_id = Column(
+        "cultivation_id", Integer, ForeignKey("cultivation.cultivation_id")
+    )
+    fertilizer_id = Column(
+        "fertilizer_id", Integer, ForeignKey("fertilizer.fertilizer_id")
+    )
+    amount = Column("amount", Float(asdecimal=True))
+    measure = Column("measure", Enum(MeasureType))
+    month = Column("month", Integer)
+    fields = relationship(
+        "Field",
+        secondary=field_fertilization,
+        back_populates="fertilizations",
+    )
+    cultivation = relationship("Cultivation", backref=backref("fertilization"))
+    fertilizer = relationship("Fertilizer", backref=backref("fertilization"))
+
+    def __repr__(self):
+        return (
+            f"Fertilization(id='{self.id}', name='{self.fertilizer.name}', amount='{self.amount:.2f}', "
+            f"measure='{self.measure.name}', month='{self.month}', crop='{self.cultivation.crop.name}')"
+        )
 
 
 class Crop(Base):
     __tablename__ = "crop"
     id = Column("crop_id", Integer, primary_key=True)
     name = Column("name", String, unique=True)
+    crop_type = Column("type", String)
 
     def __repr__(self):
-        return f"Crop(id='{self.id}', name='{self.name}')"
-
-
-class Fertilization(Base):
-    __tablename__ = "fertilization"
-    id = Column("fertilization_id", Integer, primary_key=True)
-    cultivated_crop_id = Column(
-        "cultivated_crop_id", Integer, ForeignKey("cultivated_crop.cultivated_crop_id")
-    )
-    fertilizer_id = Column(
-        "fertilizer_id", Integer, ForeignKey("fertilizer.fertilizer_id")
-    )
-    amount = Column("amount", Float(asdecimal=True))
-    measure = Column("measure", String)
-    month = Column("month", Integer)
-    cultivations = relationship(
-        "Cultivation",
-        secondary=cultivation_fertilization,
-        back_populates="fertilizations",
-    )
-    crop = relationship("CultivatedCrop", backref=backref("fertilization"))
-    name = relationship("Fertilizer", backref=backref("fertilization"))
-
-    def __repr__(self):
-        return f"Fertilization(id='{self.id}', name='{self.name.name}', amount='{self.amount:.2f}', measure='{self.measure}', month='{self.month}', crop='{self.crop.crop.name}')"
+        return f"Crop(id='{self.id}', name='{self.name}', type='{self.crop_type}')"
 
 
 class Fertilizer(Base):
@@ -137,8 +210,32 @@ class Fertilizer(Base):
     id = Column("fertilizer_id", Integer, primary_key=True)
     name = Column("name", String, unique=True)
     year = Column("year", Integer)
-    type = Column("type", String)
+    fert_class = Column("class", Enum(FertClass))
+    fert_type = Column("type", Enum(FertType))
     active = Column("active", Boolean, nullable=True)
 
     def __repr__(self):
-        return f"Fertilizer(id='{self.id}', name='{self.name}', year='{self.year}', type='{self.type}', active='{self.active}')"
+        return (
+            f"Fertilizer(id='{self.id}', name='{self.name}', year='{self.year}', "
+            f"class='{self.fert_class.name}', type='{self.fert_type.name}', active='{self.active}')"
+        )
+
+
+class SoilSample(Base):
+    __tablename__ = "soil_sample"
+    id = Column("sample_id", Integer, primary_key=True)
+    field_id = Column("field_id", Integer, ForeignKey("field.field_id"))
+    date = Column("date", Integer)
+    ph = Column("ph", Float(asdecimal=True))
+    p2o5 = Column("p2o5", Float(asdecimal=True))
+    k2o = Column("k2o", Float(asdecimal=True))
+    mg = Column("mg", Float(asdecimal=True))
+    soil_type = Column("soil_type", Enum(SoilType))
+    humus = Column("humus", Enum(HumusType))
+    field = relationship("Field", backref=backref("soil_sample"))
+
+    def __repr__(self):
+        return (
+            f"SoilSample(id='{self.id}', fields='{self.field.name}', year='{self.year}', "
+            f"soil_type='{self.soil_type.name}', humus='{self.humus.name}')"
+        )
