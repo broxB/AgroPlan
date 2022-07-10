cbe56c4524fdc2b34890218896ef9491e9034869
broxB <philippbrockmann@gmail.com>
2022-07-10 21:33:11 +0200

    Remove planning module from model package init

--- app/model/__init__.py
+++ app/model/__init__.py
@@ -3,5 +3,4 @@ from .cultivation import Cultivation
 from .fertilization import Fertilization
 from .fertilizer import Fertilizer
 from .field import Field
-from .planning import Plan
 from .soil import Soil
