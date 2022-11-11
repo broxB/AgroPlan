b48a31405fd81e8513fb83115a7fcc419d94f5c3
broxB <philippbrockmann@gmail.com>
2022-11-11 10:40:15 +0100

    Add error catching for sidebar scroll positioning

--- app/static/js/sidebar.js
+++ app/static/js/sidebar.js
@@ -9,19 +9,19 @@ window.addEventListener("load", () => {
 });
 // save scroll position of sidebar
 window.addEventListener("beforeunload", () => {
-  localStorage.setItem(
-    "scrollPositon",
-    document.querySelector("#sidebar").scrollTop
-  );
+  localStorage.setItem("scrollPositon", document.querySelector("#sidebar").scrollTop);
 });
 
 // scroll to selected field and add active tag
 function activateSidebarItem() {
-  const value = JSON.parse(document.getElementById("data").textContent);
-  var element = document.getElementById(value);
-  element.classList.add("active");
-  document.querySelector("#sidebar").scrollTop =
-    localStorage.getItem("scrollPositon") || 0;
+  try {
+    const value = JSON.parse(document.getElementById("data").textContent);
+    var element = document.getElementById(value);
+    element.classList.add("active");
+    document.querySelector("#sidebar").scrollTop = localStorage.getItem("scrollPositon") || 0;
+  } catch (err) {
+    document.querySelector("#sidebar").scrollTop = 0;
+  }
 }
 // toggle sidebar in mobile mode
 function toggleSidebar(toggler) {
