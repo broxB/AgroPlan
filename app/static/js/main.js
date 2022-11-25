c86528d89c35f771fcbebd67bd335a500b98af30
broxB <philippbrockmann@gmail.com>
2022-11-25 20:10:58 +0100

    Change alerts to toast with nav-item to redisplay

---
+++ app/static/js/main.js
@@ -0,0 +1,41 @@
+window.addEventListener("load", () => {
+  var toastTrigger = document.getElementById("resetToast");
+  var toastList = document.getElementsByClassName("toast");
+  toastTrigger.addEventListener("click", () => {
+    showToast(toastList);
+    return true;
+  });
+});
+
+function showToast(toastList) {
+  for (let toast of toastList) {
+    toast.classList.remove("hide");
+    toast.classList.add("show");
+  }
+}
+
+// function showToast(toastList, timeout) {
+//   for (let toastItem of toastList) {
+//     var options = timeout ? { delay: timeout } : {};
+//     var toast = new bootstrap.Toast(toastItem, options);
+//     var bar = toastItem.querySelector(".progress-bar");
+//     // bootstrap.Toast.Default.delay = timeout;
+//     toast.show();
+//     if (timeout) {
+//       startProgress(bar, timeout);
+//     }
+//   }
+// }
+
+// function startProgress(bar, timeout) {
+//   var width = 100;
+//   var interval = timeout / 100;
+//   var id = setInterval(() => {
+//     if (width > 0) {
+//       width--;
+//       bar.style.width = `${width}%`;
+//     } else {
+//       clearInterval(id);
+//     }
+//   }, interval);
+// }
