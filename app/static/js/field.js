86430c4bc4338cece68b0c4d9433017a9eb9ee04
broxB <philippbrockmann@gmail.com>
2022-12-04 23:45:02 +0100

    Change field editing test: - change col width - remove commented out html + css - move input type description to thead - refactor js for better SOLID compliance - add input type fetching from thead to js

--- app/static/js/field.js
+++ app/static/js/field.js
@@ -3,13 +3,11 @@ window.addEventListener("load", () => {
   var id = localStorage.getItem("activeTab");
   if (id) {
     var elem = document.querySelector(`${id}-tab`);
-    var tab = new bootstrap.Tab(elem);
-    tab.show();
   } else {
     var elem = document.querySelector("#field-nav > button");
-    var tab = new bootstrap.Tab(elem);
-    tab.show();
   }
+  var tab = new bootstrap.Tab(elem);
+  tab.show();
 });
 
 // save tab selection on click
@@ -26,14 +24,15 @@ var fallback = [];
 function edit(element) {
   var tr = element.parentElement.parentElement;
   if (!tr.classList.contains("editing")) {
+    fallback = [];
     tr.classList.toggle("editing");
     var td = tr.getElementsByTagName("td");
     for (let elem of td) {
       if (!elem.classList.contains("action")) {
         const value = elem.textContent.trim();
+        fallback.push(value);
         elem.textContent = "";
-        const elemType = elem.classList[0];
-        const input = createElement(elemType, value);
+        const input = createInput(elem, value);
         elem.appendChild(input);
       } else {
         toggleButtons(elem);
@@ -48,12 +47,11 @@ function save(element) {
   var td = tr.getElementsByTagName("td");
   for (let elem of td) {
     if (!elem.classList.contains("action")) {
-      elem.textContent = newValue(elem);
+      elem.textContent = getNewValue(elem);
     } else {
       toggleButtons(elem);
     }
   }
-  fallback = [];
 }
 
 function cancel(element) {
@@ -67,10 +65,10 @@ function cancel(element) {
       toggleButtons(elem);
     }
   }
-  fallback = [];
 }
 
-function createElement(type, value) {
+function createInput(elem, value) {
+  var type = getElementType(elem);
   var input = document.createElement(type);
   if (type == "input") {
     input.type = "text";
@@ -83,19 +81,41 @@ function createElement(type, value) {
     input.add(option, input.length + 1);
     input.classList.add("form-select");
   }
-  fallback.push(value);
+  input.style.minWidth = value.length + 6 + "ch"; // adjust size to value length
   return input;
 }
 
-function newValue(elem) {
-  var inputClass = elem.classList[0];
-  if (inputClass == "input") {
-    return elem.querySelector("INPUT").value;
-  } else if (inputClass == "select") {
-    const select = elem.querySelector("SELECT");
-    const text = select.options[select.selectedIndex].text;
-    return text;
+function getNewValue(elem) {
+  var type = elem.firstChild.tagName;
+  if (type == "INPUT") {
+    var value = elem.querySelector(type).value;
+  } else if (type == "SELECT") {
+    const select = elem.querySelector(type);
+    var value = select.options[select.selectedIndex].text;
   }
+  return value;
+}
+
+function getElementType(td) {
+  // https://stackoverflow.com/a/37312707/16256581
+  const index = Array.prototype.indexOf.call(td.parentNode.children, td);
+  const corresponding_th = td.closest("table").querySelector("th:nth-child(" + (index + 1) + ")");
+  return corresponding_th.classList[0];
+
+  // https://stackoverflow.com/a/46139306/16256581 for col-span support
+  /*
+  var idx = [...td.parentNode.children].indexOf(td), // get td index
+      thCells = td.closest('table').tHead.rows[0].cells, // get all th cells
+      th_colSpan_acc = 0 // accumulator
+  // iterate all th cells and add-up their colSpan value
+  for( var i=0; i < thCells.length; i++ ){
+     th_colSpan_acc += thCells[i].colSpan
+     if( th_colSpan_acc >= (idx + td.colSpan) ) {
+      break
+    }
+  }
+  return thCells[i].classList[0]
+  */
 }
 
 function toggleButtons(elem) {
