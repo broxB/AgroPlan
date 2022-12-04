9a056c37eb5f4d41d99a6f82693cd8fd07d406cc
broxB <philippbrockmann@gmail.com>
2022-12-04 21:45:59 +0100

    Test inline form with bootstrap table

--- app/static/js/field.js
+++ app/static/js/field.js
@@ -1,3 +1,4 @@
+// restore selected tab on reload
 window.addEventListener("load", () => {
   var id = localStorage.getItem("activeTab");
   if (id) {
@@ -11,6 +12,7 @@ window.addEventListener("load", () => {
   }
 });
 
+// save tab selection on click
 document.querySelectorAll("#field-nav > button").forEach((btn) => {
   btn.addEventListener("shown.bs.tab", () => {
     var id = btn.dataset["bsTarget"];
@@ -18,20 +20,22 @@ document.querySelectorAll("#field-nav > button").forEach((btn) => {
   });
 });
 
+// global for original values
+var fallback = [];
+
 function edit(element) {
   var tr = element.parentElement.parentElement;
   if (!tr.classList.contains("editing")) {
     tr.classList.toggle("editing");
-    var td = tr.getElementsByClassName("form-td");
+    var td = tr.getElementsByTagName("td");
     for (let elem of td) {
       if (!elem.classList.contains("action")) {
-        var value = elem.textContent;
+        const value = elem.textContent.trim();
         elem.textContent = "";
-        var input = createElement("input", value);
+        const elemType = elem.classList[0];
+        const input = createElement(elemType, value);
         elem.appendChild(input);
       } else {
-        // elem.querySelector("I").classList = "fa-solid fa-square-check";
-        // elem.querySelector("I").classList = "fa-solid fa-pen-to-square";
         toggleButtons(elem);
       }
     }
@@ -41,37 +45,57 @@ function edit(element) {
 function save(element) {
   var tr = element.parentElement.parentElement;
   tr.classList.toggle("editing");
-  var td = tr.getElementsByClassName("form-td");
+  var td = tr.getElementsByTagName("td");
   for (let elem of td) {
     if (!elem.classList.contains("action")) {
-      var input = elem.querySelector("INPUT");
-      elem.textContent = input.value;
+      elem.textContent = newValue(elem);
     } else {
       toggleButtons(elem);
     }
   }
+  fallback = [];
 }
 
 function cancel(element) {
   var tr = element.parentElement.parentElement;
   tr.classList.toggle("editing");
-  var td = tr.getElementsByClassName("form-td");
+  var td = tr.getElementsByTagName("td");
   for (let elem of td) {
     if (!elem.classList.contains("action")) {
-      var input = elem.querySelector("INPUT");
-      elem.textContent = input.getAttribute("fallback");
+      elem.textContent = fallback.shift();
     } else {
       toggleButtons(elem);
     }
   }
+  fallback = [];
 }
 
 function createElement(type, value) {
   var input = document.createElement(type);
-  input.type = "text";
-  input.setAttribute("value", value);
-  input.setAttribute("fallback", value);
+  if (type == "input") {
+    input.type = "text";
+    input.classList.add("form-control");
+    input.setAttribute("value", value);
+  } else if (type == "select") {
+    const option = document.createElement("option");
+    option.value = input.length + 1; // sets invisible id
+    option.text = value; // sets displayed text
+    input.add(option, input.length + 1);
+    input.classList.add("form-select");
+  }
+  fallback.push(value);
   return input;
+}
+
+function newValue(elem) {
+  var inputClass = elem.classList[0];
+  if (inputClass == "input") {
+    return elem.querySelector("INPUT").value;
+  } else if (inputClass == "select") {
+    const select = elem.querySelector("SELECT");
+    const text = select.options[select.selectedIndex].text;
+    return text;
+  }
 }
 
 function toggleButtons(elem) {
