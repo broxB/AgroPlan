7e55393c7857fe36c63a4bf504b73792cf58e5ba
broxB <philippbrockmann@gmail.com>
2023-01-04 09:26:57 +0100

    Delete PyQt relevant directories

--- app/gui/utils.py
+++
@@ -1,9 +0,0 @@
-import re
-from collections import namedtuple
-
-
-def split_meta_name(meta_name: str) -> list:
-    pattern = f"(\d+)-(\d+)\s(.+)\s\("
-    matches = re.findall(pattern, meta_name)[0]
-    Field = namedtuple("Field", "prefix, suffix, name")
-    return Field(int(matches[0]), int(matches[1]), matches[2])
