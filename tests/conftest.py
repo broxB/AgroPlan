a074d975e54dec2008cf5b2a06daecb9a28661b6
broxB <philippbrockmann@gmail.com>
2023-01-23 23:26:09 +0100

    Add full test coverage for database/model.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,7 +15,23 @@ from app.database.model import (
     SoilSample,
     User,
 )
-from app.database.types import DemandType, FieldType
+from app.database.types import (
+    CropClass,
+    CropType,
+    CultivationType,
+    CutTiming,
+    DemandType,
+    FertClass,
+    FertType,
+    FieldType,
+    HumusType,
+    LegumeType,
+    MeasureType,
+    NminType,
+    ResidueType,
+    SoilType,
+    UnitType,
+)
 from app.extensions import db as _db
 from config import TestConfig
 
@@ -45,7 +61,7 @@ def db(client):
 def user(db) -> User:
     user = User(username="Test", email="test@test.test")
     user.set_password("ValidPassword")
-    user.year = 2000
+    user.year = 1000
     db.session.add(user)
     db.session.commit()
     return user
@@ -54,6 +70,7 @@ def user(db) -> User:
 @pytest.fixture
 def base_field(db, user) -> BaseField:
     base_field = BaseField(user_id=user.id, prefix=1, suffix=0, name="Testfield")
+    base_field.user = user
     db.session.add(base_field)
     db.session.commit()
     return base_field
@@ -70,6 +87,147 @@ def field(db, base_field) -> BaseField:
         field_type=FieldType.cropland,
         demand_type=DemandType.demand,
     )
+    field.base_field = base_field
     db.session.add(field)
     db.session.commit()
     return field
+
+
+@pytest.fixture
+def crop(db, user) -> Crop:
+    crop = Crop(
+        user_id=user.id,
+        name="Testcrop",
+        field_type=FieldType.cropland,
+        crop_class=CropClass.main_crop,
+        crop_type=CropType.corn,
+        kind="Test",
+        feedable=False,
+        residue=False,
+        legume_rate=LegumeType.none,
+        nmin_depth=NminType.nmin_90,
+        target_demand=Decimal(100),
+        target_yield=Decimal(100),
+        pos_yield=Decimal(1),
+        neg_yield=Decimal(2),
+        target_protein=Decimal(),
+        var_protein=Decimal(),
+        n=Decimal(1),
+        p2o5=Decimal(1),
+        k2o=Decimal(1),
+        mgo=Decimal(1),
+    )
+    db.session.add(crop)
+    db.session.commit()
+    return crop
+
+
+@pytest.fixture
+def cultivation(db, crop, field) -> Cultivation:
+    cultivation = Cultivation(
+        field_id=field.id,
+        cultivation_type=CultivationType.main_crop,
+        crop_id=crop.id,
+        crop_yield=Decimal(110),
+        crop_protein=Decimal(),
+        residues=ResidueType.main_no_residues,
+        legume_rate=LegumeType.none,
+        nmin_30=10,
+        nmin_60=10,
+        nmin_90=0,
+    )
+    cultivation.crop = crop
+    cultivation.field = field
+    db.session.add(cultivation)
+    db.session.commit()
+    return cultivation
+
+
+@pytest.fixture
+def fertilizer(db, user) -> Fertilizer:
+    fertilizer = Fertilizer(
+        user_id=user.id,
+        name="Testfertilizer",
+        year=1000,
+        fert_class=FertClass.organic,
+        fert_type=FertType.org_digestate,
+        active=True,
+        unit=UnitType.cbm,
+        price=Decimal(100),
+        n=Decimal(1),
+        p2o5=Decimal(1),
+        k2o=Decimal(1),
+        mgo=Decimal(1),
+        s=Decimal(1),
+        cao=Decimal(1),
+        nh4=Decimal(1),
+    )
+    db.session.add(fertilizer)
+    db.session.commit()
+    return fertilizer
+
+
+@pytest.fixture
+def fertilization(db, field, cultivation, fertilizer) -> Fertilization:
+    fertilization = Fertilization(
+        cultivation_id=cultivation.id,
+        fertilizer_id=fertilizer.id,
+        cut_timing=CutTiming.non_mowable,
+        amount=Decimal(10),
+        measure=MeasureType.org_fall,
+        month=10,
+    )
+    fertilization.fertilizer = fertilizer
+    fertilization.cultivation = cultivation
+    fertilization.field.append(field)
+    db.session.add(fertilization)
+    db.session.commit()
+    return fertilization
+
+
+@pytest.fixture
+def soil_sample(db, base_field, field) -> SoilSample:
+    soil_sample = SoilSample(
+        base_id=base_field.id,
+        year=1000,
+        ph=Decimal(1),
+        p2o5=Decimal(1),
+        k2o=Decimal(1),
+        mg=Decimal(1),
+        soil_type=SoilType.sand,
+        humus=HumusType.less_8,
+    )
+    soil_sample.fields.append(field)
+    db.session.add(soil_sample)
+    db.session.commit()
+    return soil_sample
+
+
+@pytest.fixture
+def fertilizer_usage(db, user, fertilizer, fertilization, field) -> FertilizerUsage:
+    fertilizer_usage = FertilizerUsage(
+        user_id=user.id,
+        name=fertilizer.name,
+        year=field.year,
+        amount=field.area * fertilization.amount,
+    )
+    db.session.add(fertilizer_usage)
+    db.session.commit()
+    return fertilizer_usage
+
+
+@pytest.fixture
+def saldo(db, field) -> Saldo:
+    saldo = Saldo(
+        field_id=field.id,
+        n=Decimal(1),
+        p2o5=Decimal(1),
+        k2o=Decimal(1),
+        mgo=Decimal(1),
+        s=Decimal(1),
+        cao=Decimal(1),
+        n_total=Decimal(1),
+    )
+    db.session.add(saldo)
+    db.session.commit()
+    return saldo
--- tests/functional/test_model_functions.py
+++ tests/functional/test_model_functions.py
@@ -1,16 +1,41 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from time import time
 
-from app.database.model import BaseField, Field, User
+from jwt import encode
+
+from app.database.model import BaseField, Crop, Fertilizer, FertilizerUsage, Field, User
+from app.database.types import CropClass, FertClass, FieldType
 
 
-def test_user_reset_password(user: User, client: Flask.test_client, db: SQLAlchemy):
+def test_user_reset_password(user: User, client):
     token = user.get_reset_password_token()
-    verified_user = user.verify_reset_password_token(token)
-    assert verified_user == user
+    false_token = encode(
+        {"reset_password": user.id, "exp": time()}, "SECRET_KEY", algorithm="HS256"
+    )
+    verify_token = user.verify_reset_password_token
+    assert verify_token(token) == user
+    assert verify_token(false_token) is None
 
 
-def test_user_get_fields(
-    user: User, base_field: BaseField, client: Flask.test_client, db: SQLAlchemy
-):
-    assert user.get_fields().all() == [base_field]
+def test_user_get_fields(user: User, base_field: BaseField, field: Field, client):
+    assert base_field in user.get_fields().all()
+    assert base_field in user.get_fields(year=field.year).all()
+
+
+def test_user_get_years(user: User, field: Field, client):
+    assert field.year in user.get_years()
+
+
+def test_user_get_crops(user: User, crop: Crop, client):
+    assert crop in user.get_crops()
+    assert crop in user.get_crops(crop_class=crop.crop_class)
+    assert crop in user.get_crops(field_type=crop.field_type)
+
+
+def test_user_get_fertilizers(user: User, fertilizer: Fertilizer, client):
+    assert fertilizer in user.get_fertilizers()
+    assert fertilizer in user.get_fertilizers(fert_class=fertilizer.fert_class)
+    assert fertilizer in user.get_fertilizers(year=fertilizer.year)
+
+
+def test_user_get_fertilizer_usage(user: User, fertilizer_usage: FertilizerUsage, client):
+    assert fertilizer_usage in user.get_fertilizer_usage()
