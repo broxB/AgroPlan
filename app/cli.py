edc2a499531c05162d473b2f01427ac5296a5e6a
broxB <philippbrockmann@gmail.com>
2023-01-27 21:45:19 +0100

    Add pytest cov as flask cli command

--- app/cli.py
+++ app/cli.py
@@ -1,3 +1,5 @@
+import os
+
 import click
 
 from app.database.setup import setup_database
@@ -7,9 +9,15 @@ from app.utils import load_json
 def register(app):
     @app.cli.command("seed")
     def seed():
-        """Seed database"""
+        """Seed database with sample data"""
         fields = load_json("data/schläge_reversed.json")
         fertilizers = load_json("data/dünger.json")
         crops = load_json("data/kulturen.json")
         seed = [fields, fertilizers, crops]
         setup_database(seed=seed)
+
+    @app.cli.command("pytest")
+    def pytest():
+        """Start pytest with missing coverage report"""
+        if os.system("pytest --cov-report term-missing --cov"):
+            raise RuntimeError("invalid pytest command")
