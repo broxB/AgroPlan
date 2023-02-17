9744402f93d2b62bea8d3e39dcfb107068655e1e
broxB <philippbrockmann@gmail.com>
2023-02-17 21:40:20 +0100

    Change seed cli to take file as source for seeding

--- app/cli.py
+++ app/cli.py
@@ -1,15 +1,35 @@
+import io
+import json
 import os
 
 import click
 
 from app.database.setup import setup_database
-from app.utils import load_json
+from app.utils import load_json, save_json
+from app.utils.utils import renew_dict
 
 
 def register(app):
-    @app.cli.command("seed")
+    @app.cli.group()
     def seed():
-        """Seed database with sample data"""
+        """Seed database with sample data."""
+
+    @seed.command()
+    @click.argument("path")
+    def init(path: str):
+        """Make excel json export compatible with database seeding"""
+        try:
+            new_dict: dict = json.load(io.open(path, "r", encoding="utf-8-sig"))
+            reversed_dict = {k: new_dict[k] for k in list(reversed(new_dict.keys()))}
+            labeled_dict = renew_dict(reversed_dict)
+            save_json(labeled_dict, "data/schläge_reversed.json")
+            print("Successfully generated file.")
+        except FileNotFoundError:
+            print("No valid file provided.")
+
+    @seed.command()
+    def apply():
+        """Load generated jsons and seed database."""
         fields = load_json("data/schläge_reversed.json")
         fertilizers = load_json("data/dünger.json")
         crops = load_json("data/kulturen.json")
