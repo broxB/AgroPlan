566f538a00d4a5c51f793fade350facc4c798f05
broxB <philippbrockmann@gmail.com>
2023-03-06 21:31:59 +0100

    Fix tests associated with new balance class

--- tests/model/test_crop.py
+++ tests/model/test_crop.py
@@ -2,7 +2,7 @@ from decimal import Decimal
 
 import pytest
 
-from app.model.crop import Crop
+from app.model import Balance, Crop
 
 
 @pytest.fixture
@@ -13,10 +13,11 @@ def crop(crop) -> Crop:
 @pytest.mark.parametrize(
     "crop_yield, crop_protein, expected",
     [
-        (Decimal("100"), Decimal("16"), [100, 100, 100, 100, 20, 0]),
-        (Decimal("110"), Decimal("16.5"), [Decimal("110.75"), 110, 110, 110, 20, 0]),
-        (Decimal("90"), Decimal("15.5"), [Decimal("79.25"), 90, 90, 90, 20, 0]),
+        (Decimal("100"), Decimal("16"), Balance("", 100, 100, 100, 100, 20, 0)),
+        (Decimal("110"), Decimal("16.5"), Balance("", Decimal("110.75"), 110, 110, 110, 20, 0)),
+        (Decimal("90"), Decimal("15.5"), Balance("", Decimal("79.25"), 90, 90, 90, 20, 0)),
     ],
+    ids=["higher yield", "normal yield", "lower yield"],
 )
 def test_demand_crop(crop: Crop, crop_yield, crop_protein, expected):
     assert crop.demand_crop(crop_yield, crop_protein) == expected
@@ -25,10 +26,11 @@ def test_demand_crop(crop: Crop, crop_yield, crop_protein, expected):
 @pytest.mark.parametrize(
     "crop_yield, expected",
     [
-        (Decimal("100"), [0, 40, 40, 40, 0, 0]),
-        (Decimal("110"), [0, 44, 44, 44, 0, 0]),
-        (Decimal("90"), [0, 36, 36, 36, 0, 0]),
+        (Decimal("100"), Balance("", 0, 40, 40, 40, 0, 0)),
+        (Decimal("110"), Balance("", 0, 44, 44, 44, 0, 0)),
+        (Decimal("90"), Balance("", 0, 36, 36, 36, 0, 0)),
     ],
+    ids=["higher yield", "normal yield", "lower yield"],
 )
 def test_demand_byproduct(crop: Crop, crop_yield, expected):
     assert crop.demand_byproduct(crop_yield) == expected
@@ -45,6 +47,7 @@ def test_s_demand(crop: Crop):
         (Decimal("110"), Decimal("16.5"), Decimal("110.75")),
         (Decimal("90"), Decimal("15.5"), Decimal("79.25")),
     ],
+    ids=["higher yield", "normal yield", "lower yield"],
 )
 def test__n_demand(crop: Crop, crop_yield, crop_protein, expected):
     assert crop._n_demand(crop_yield, crop_protein) == expected
@@ -57,6 +60,7 @@ def test__n_demand(crop: Crop, crop_yield, crop_protein, expected):
         (Decimal("110"), Decimal("0.9"), Decimal("99")),
         (Decimal("90"), Decimal("0.9"), Decimal("81")),
     ],
+    ids=["higher yield", "normal yield", "lower yield"],
 )
 def test__nutrient(crop: Crop, crop_yield, nutrient, expected):
     assert crop._nutrient(crop_yield, nutrient) == expected
@@ -69,6 +73,11 @@ def test__nutrient(crop: Crop, crop_yield, nutrient, expected):
         (Decimal("110"), Decimal("0.9"), Decimal("79.2")),
         (Decimal("90"), Decimal("0.9"), Decimal("64.8")),
     ],
+    ids=["higher yield", "normal yield", "lower yield"],
 )
 def test__nutrient_byproduct(crop: Crop, crop_yield, byp_nutrient, expected):
     assert crop._nutrient_byproduct(crop_yield, byp_nutrient) == expected
+
+
+def test_repr(crop: Crop):
+    assert crop.name in str(crop.__repr__)
--- tests/model/test_cultivation.py
+++ tests/model/test_cultivation.py
@@ -9,7 +9,7 @@ from app.database.types import (
     NminType,
     ResidueType,
 )
-from app.model.crop import Crop
+from app.model import Balance, Crop
 from app.model.cultivation import (
     CatchCrop,
     Cultivation,
@@ -28,12 +28,15 @@ def test_create_cultivation(cultivation: db.Cultivation, test_crop: Crop):
     cultivation.cultivation_type = CultivationType.main_crop
     main_cult = create_cultivation(cultivation, test_crop)
     assert isinstance(main_cult, MainCrop)
+    assert "Main crop" in str(main_cult.__repr__)
     cultivation.cultivation_type = CultivationType.catch_crop
     catch_cult = create_cultivation(cultivation, test_crop)
     assert isinstance(catch_cult, CatchCrop)
+    assert "Catch crop" in str(catch_cult.__repr__)
     cultivation.cultivation_type = CultivationType.second_crop
     second_cult = create_cultivation(cultivation, test_crop)
     assert isinstance(second_cult, SecondCrop)
+    assert "Second crop" in str(second_cult.__repr__)
 
 
 @pytest.fixture
@@ -42,18 +45,25 @@ def test_cultivation(cultivation, test_crop) -> Cultivation:
 
 
 def test_demand(test_cultivation: Cultivation):
-    assert test_cultivation.demand(DemandType.demand) == [-110, -154, -154, -154, -20, -0]
-    assert test_cultivation.demand(DemandType.removal) == [-110, -110, -110, -110, -20, -0]
+    assert test_cultivation.demand(DemandType.demand) == Balance(
+        "Crop demand", -110, -154, -154, -154, -20, -0
+    )
+    assert test_cultivation.demand(DemandType.removal) == Balance(
+        "Crop demand", -110, -110, -110, -110, -20, -0
+    )
     test_cultivation.residues = ResidueType.main_removed
-    assert test_cultivation.demand(DemandType.removal) == [-110, -154, -154, -154, -20, -0]
-    assert test_cultivation.demand(DemandType.demand, negative_output=False) == [
+    assert test_cultivation.demand(DemandType.removal) == Balance(
+        "Crop demand", -110, -154, -154, -154, -20, -0
+    )
+    assert test_cultivation.demand(DemandType.demand, negative_output=False) == Balance(
+        "Crop demand",
         110,
         154,
         154,
         154,
         20,
         0,
-    ]
+    )
 
 
 def test_pre_crop_effect(test_cultivation: Cultivation):
@@ -130,7 +140,8 @@ def catch_crop(cultivation, test_crop) -> CatchCrop:
 
 
 def test_catch_crop_demand(catch_crop: CatchCrop):
-    assert catch_crop.demand() == [-60, 0, 0, 0, 0, 0]
+    assert catch_crop.demand() == Balance("Crop demand", -60, 0, 0, 0, 0, 0)
+    assert catch_crop.demand(negative_output=False) == Balance("Crop demand", 60, 0, 0, 0, 0, 0)
 
 
 @pytest.mark.parametrize(
@@ -139,8 +150,9 @@ def test_catch_crop_demand(catch_crop: CatchCrop):
         (ResidueType.catch_frozen, 10),
         (ResidueType.catch_not_frozen_spring, 40),
         (ResidueType.catch_not_frozen_fall, 10),
-        (ResidueType.catch_frozen, 10),
+        (ResidueType.catch_used, 10),
     ],
+    ids=["frozen", "not frozen, spring", "not frozen fall", "used"],
 )
 def test_catch_crop_pre_crop_effect(catch_crop: CatchCrop, residues, expected):
     catch_crop.crop_type = CropType.catch_legume
