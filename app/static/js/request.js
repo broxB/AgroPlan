b448f68be743e0a617437352f8a7e0de08fd0e31
broxB <philippbrockmann@gmail.com>
2023-06-16 00:19:59 +0200

    feat: use fetch api for POST and GET request

---
+++ app/static/js/request.js
@@ -0,0 +1,20 @@
+export async function fetchData(endpoint) {
+  return fetch(endpoint)
+    .then((response) => response.json())
+    .catch((err) => {
+      console.log(err);
+      return err;
+    });
+}
+
+export async function sendForm(form, action, endpoint) {
+  const formData = new FormData(form);
+  const dataJSON = JSON.stringify(Object.fromEntries(formData));
+  const headers = new Headers([["Content-Type", "application/json"]]);
+  return await fetch(endpoint, { method: action, body: dataJSON, headers: headers })
+    .then((response) => response.json())
+    .catch((err) => {
+      console.log(err);
+      return err;
+    });
+}
