5416cbbfa16dcd592c9de2e4cd6488935520338c
broxB <philippbrockmann@gmail.com>
2023-06-23 00:51:37 +0200

    feat: expose response handling of fetch functions

--- app/static/js/request.js
+++ app/static/js/request.js
@@ -1,16 +1,28 @@
-export async function fetchData(endpoint) {
+export async function fetchData(endpoint, full_response = false) {
   return fetch(endpoint)
-    .then((response) => response.json())
+    .then((response) => {
+      if (!full_response) {
+        return response.json();
+      } else {
+        return response;
+      }
+    })
     .catch((err) => {
       console.log(err);
       return err;
     });
 }
 
-export async function sendForm(form, action, endpoint) {
+export async function sendForm(form, action, endpoint, full_response = false) {
   const formData = new FormData(form);
   return fetch(endpoint, { method: action, body: formData })
-    .then((response) => response.json())
+    .then((response) => {
+      if (!full_response) {
+        return response.json();
+      } else {
+        return response;
+      }
+    })
     .catch((err) => {
       console.log(err);
       return err;
