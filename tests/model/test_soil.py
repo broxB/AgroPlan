caad5a84c817067ea5a4e8e9f703151614b8a1d1
broxB <philippbrockmann@gmail.com>
2024-05-22 17:16:46 +0200

    test: add tests in soil for key and index errors for accessing guideline dicts

--- tests/model/test_soil.py
+++ tests/model/test_soil.py
@@ -7,6 +7,68 @@ from app.database.types import FieldType, HumusType, SoilType
 from app.model.soil import Soil, create_soil_sample
 
 
+@pytest.fixture
+def empty_guidelines():
+    class guidelines:
+        @staticmethod
+        def soil_reductions():
+            return {}
+
+        @staticmethod
+        def p2o5_reductions():
+            return {}
+
+        @staticmethod
+        def k2o_reductions():
+            return {}
+
+        @staticmethod
+        def mg_reductions():
+            return {}
+
+        @staticmethod
+        def sulfur_reductions():
+            return {}
+
+        @staticmethod
+        def cao_reductions():
+            return {}
+
+        @staticmethod
+        def p2o5_classes():
+            return {}
+
+        @staticmethod
+        def k2o_classes():
+            return {}
+
+        @staticmethod
+        def mg_classes():
+            return {}
+
+        @staticmethod
+        def ph_classes():
+            return {}
+
+        @staticmethod
+        def org_factor():
+            return {}
+
+        @staticmethod
+        def pre_crop_effect():
+            return {}
+
+        @staticmethod
+        def legume_delivery():
+            return {}
+
+        @staticmethod
+        def sulfur_needs():
+            return {}
+
+    return guidelines
+
+
 def test_create_soil_sample(field_first_year: Field, guidelines, fill_db):
     soil = create_soil_sample(
         field_first_year.soil_samples,
@@ -28,12 +90,23 @@ def soil(soil_sample: SoilSample, field_first_year: Field, guidelines) -> Soil:
     return Soil(soil_sample, field_first_year.field_type, guidelines=guidelines)
 
 
+@pytest.fixture
+def empty_soil(soil_sample: SoilSample, field_first_year: Field, empty_guidelines) -> Soil:
+    soil_sample.humus = HumusType.less_4
+    soil_sample.soil_type = SoilType.sand
+    return Soil(soil_sample, field_first_year.field_type, guidelines=empty_guidelines)
+
+
 @pytest.mark.parametrize(
     "field_type, expected", [(FieldType.cropland, 0), (FieldType.grassland, 10)]
 )
 def test_reduction_n(soil: Soil, field_type, expected):
     soil.field_type = field_type
     assert soil.reduction_n() == expected
+
+
+def test_reduction_n_error(empty_soil: Soil):
+    assert empty_soil.reduction_n() == 0
 
 
 @pytest.mark.parametrize(
@@ -44,9 +117,13 @@ def test_reduction_p2o5(soil: Soil, field_type, expected):
     assert soil.reduction_p2o5() == expected
 
 
-def test_reduction_p2o5_without_parametrize(soil: Soil):
+def test_reduction_p2o5_none_value(soil: Soil):
     soil.p2o5 = None
     assert soil.reduction_p2o5() == Decimal()
+
+
+def test_reduction_p2o5_error(empty_soil: Soil):
+    assert empty_soil.reduction_p2o5() == 0
 
 
 @pytest.mark.parametrize(
@@ -57,9 +134,13 @@ def test_reduction_k2o(soil: Soil, field_type, expected):
     assert soil.reduction_k2o() == expected
 
 
-def test_reduction_k2o_without_parametrize(soil: Soil):
+def test_reduction_k2o_none_value(soil: Soil):
     soil.k2o = None
     assert soil.reduction_k2o() == Decimal()
+
+
+def test_reduction_k2o_error(empty_soil: Soil):
+    assert empty_soil.reduction_k2o() == 0
 
 
 @pytest.mark.parametrize(
@@ -70,9 +151,13 @@ def test_reduction_mg(soil: Soil, field_type, expected):
     assert soil.reduction_mg() == expected
 
 
-def test_reduction_mg_without_parametrize(soil: Soil):
+def test_reduction_mg_none_value(soil: Soil):
     soil.mg = None
     assert soil.reduction_mg() == Decimal()
+
+
+def test_reduction_mg_error(empty_soil: Soil):
+    assert empty_soil.reduction_mg() == 0
 
 
 @pytest.mark.parametrize(
@@ -83,7 +168,7 @@ def test_reduction_cao(soil: Soil, field_type, expected):
     assert soil.reduction_cao() == expected
 
 
-def test_reduction_cao_without_parametrize(soil: Soil):
+def test_reduction_cao_none_value(soil: Soil):
     assert soil.reduction_cao(preservation=True) == -150
     soil.ph = None
     assert soil.reduction_cao() == Decimal()
@@ -91,8 +176,16 @@ def test_reduction_cao_without_parametrize(soil: Soil):
     assert soil.reduction_cao() == Decimal()
 
 
+def test_reduction_cao_error(empty_soil: Soil):
+    assert empty_soil.reduction_cao() == 0
+
+
 def test_reduction_s(soil: Soil):
     assert soil.reduction_s(s_demand=Decimal(0), n_total=(0)) == 0
+
+
+def test_reduction_s_error(empty_soil: Soil):
+    assert empty_soil.reduction_s(s_demand=0, n_total=0) == 0
 
 
 @pytest.mark.parametrize(
@@ -103,9 +196,13 @@ def test_class_p2o5(soil: Soil, field_type, expected):
     assert soil.class_p2o5() == expected
 
 
-def test_class_p2o5_without_parametrize(soil: Soil):
+def test_class_p2o5_none_value(soil: Soil):
     soil.p2o5 = None
     assert soil.class_p2o5() == ""
+
+
+def test_class_p2o5_error(empty_soil: Soil):
+    assert empty_soil.class_p2o5() == ""
 
 
 @pytest.mark.parametrize(
@@ -116,9 +213,13 @@ def test_class_k2o(soil: Soil, field_type, expected):
     assert soil.class_k2o() == expected
 
 
-def test_class_k2o_without_parametrize(soil: Soil):
+def test_class_k2o_none_value(soil: Soil):
     soil.k2o = None
     assert soil.class_k2o() == ""
+
+
+def test_class_k2o_error(empty_soil: Soil):
+    assert empty_soil.class_k2o() == ""
 
 
 @pytest.mark.parametrize(
@@ -129,9 +230,13 @@ def test_class_mg(soil: Soil, field_type, expected):
     assert soil.class_mg() == expected
 
 
-def test_class_mg_without_parametrize(soil: Soil):
+def test_class_mg_none_value(soil: Soil):
     soil.mg = None
     assert soil.class_mg() == ""
+
+
+def test_class_mg_error(empty_soil: Soil):
+    assert empty_soil.class_mg() == ""
 
 
 @pytest.mark.parametrize(
@@ -142,9 +247,13 @@ def test_class_ph(soil: Soil, field_type, expected):
     assert soil.class_ph() == expected
 
 
-def test_class_ph_without_parametrize(soil: Soil):
+def test_class_ph_none_value(soil: Soil):
     soil.ph = None
     assert soil.class_ph() == ""
+
+
+def test_class_ph_error(empty_soil: Soil):
+    assert empty_soil.class_ph() == ""
 
 
 @pytest.mark.parametrize(
@@ -154,6 +263,10 @@ def test_class_ph_without_parametrize(soil: Soil):
 def test_optimal_ph(soil: Soil, field_type, expected):
     soil.field_type = field_type
     assert soil.optimal_ph() == expected
+
+
+def test_optimal_ph_error(empty_soil: Soil):
+    assert empty_soil.optimal_ph() == 0
 
 
 @pytest.mark.parametrize(
